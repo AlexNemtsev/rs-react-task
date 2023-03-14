98408bf698747cab9ef86c747df75489df5f64e9
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-03-14 07:47:57 +0300

    feat: add styles for 404 page

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import './error-page.css';
 
 function ErrorPage(props: { onClick: (pageName: string) => void }) {
   return (
-    <main>
-      <p>
-        {"404 – Page does't exist. Go to "}
-        <Link to="/" onClick={() => props.onClick('Home')}>
+    <main className="error-page">
+      <h1 className="error-page__header">404</h1>
+      <p className="error-page__desc">
+        {'Page does not exist. Go to '}
+        <Link to="/" onClick={() => props.onClick('Home')} className="error-page__link">
           {'Home page'}
         </Link>
       </p>
