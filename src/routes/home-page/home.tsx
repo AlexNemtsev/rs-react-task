679bcdd89802d555f3c9806e554796c4ea70dec5
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-03-16 17:25:54 +0300

    feat: add search-bar

--- src/routes/home-page/home.tsx
+++ src/routes/home-page/home.tsx
@@ -6,9 +6,20 @@ function HomePage() {
   const cards = products.map((item) => <ProductCard key={item.id.toString()} item={item} />);
 
   return (
-    <section className={styles.section}>
-      <div className={styles.cards}>{cards}</div>
-    </section>
+    <>
+      <section className={styles.section}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className={styles.input}
+          placeholder="Search..."
+        />
+      </section>
+      <section className={styles.section}>
+        <div className={styles.cards}>{cards}</div>
+      </section>
+    </>
   );
 }
 
