3b47ff94faf72834b3fad971f26ff0652548c6c2
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-03-17 10:54:16 +0000

    fix: mock fixes

--- src/__tests__/location-mock.ts
+++ src/__tests__/location-mock.ts
@@ -1,17 +1,17 @@
 import { vi } from 'vitest';
 
 class LocationMock implements Location {
-  ancestorOrigins: DOMStringList;
-  hash: string;
-  host: string;
-  hostname: string;
-  href: string;
+  ancestorOrigins!: DOMStringList;
+  hash = '';
+  host = '';
+  hostname = '';
+  href = '';
   toString = vi.fn();
-  origin: string;
-  pathname: string;
-  port: string;
-  protocol: string;
-  search: string;
+  origin = '';
+  pathname = '';
+  port = '';
+  protocol = '';
+  search = '';
   assign = vi.fn();
   reload = vi.fn();
   replace = vi.fn();
