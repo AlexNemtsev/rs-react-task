0c5dc7e1e139f97afae5b283495085c47cc32c8a
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-03-24 08:00:36 +0300

    feat: add form on page

--- src/routes/form-page/form.tsx
+++ src/routes/form-page/form.tsx
@@ -2,7 +2,19 @@ import styles from './form.module.scss';
 
 const FormPage = () => {
   return (
-    <p className={styles.description}>{'The page is under construction. Please come back later'}</p>
+    <section className={styles.section}>
+      <form className={styles.form}>
+        <label>
+          First name <input type="text" name="first-name" />
+        </label>
+        <label>
+          Last name <input type="text" name="last-name" />
+        </label>
+        <label>
+          Birth date <input type="date" name="date" />
+        </label>
+      </form>
+    </section>
   );
 };
 
