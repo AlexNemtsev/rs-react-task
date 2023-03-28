0e8a6c6abb3732193dd085ce034fe0a07393e2a9
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-03-28 10:26:01 +0000

    fix: fix style

--- src/components/ConfirmMsg.tsx
+++ src/components/ConfirmMsg.tsx
@@ -2,7 +2,5 @@ import styles from './ConfirmMsg.module.scss';
 
 export default function ConfirmMsg(props: { isHide: boolean }) {
   const className = [styles.dialog, props.isHide ? styles.hide : ''].join(' ');
-  return (
-    <dialog className={className}>Card created</dialog>
-  );
+  return <dialog className={className}>Card created</dialog>;
 }
