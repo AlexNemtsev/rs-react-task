b7b0a1305c970717104d6fc5d3be3dece2939a0b
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-03-29 13:46:53 +0000

    refactor: turn FormPage into functional component

--- src/routes/form-page/Form.tsx
+++ src/routes/form-page/Form.tsx
@@ -1,15 +1,9 @@
-import React, { createRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './form.module.scss';
 import { FormRefs, FormData } from '../../interfaces/form-interfaces';
 import CvCard from '../../components/CvCard';
 import Error from '../../components/error-message/Error';
 import ConfirmMsg from '../../components/ConfirmMsg';
-
-interface FormPageState {
-  data: Array<FormData>;
-  errors: FormErrors;
-  msgHide: boolean;
-}
 
 interface FormErrors {
   [key: string]: boolean;
@@ -25,179 +19,168 @@ const positionOptions: { [key: string]: string } = {
   fs: 'Full-stack',
 };
 
-class FormPage extends React.Component<object, FormPageState> {
-  constructor(props: object) {
-    super(props);
-    this.onSubmitHandler.bind(this);
-    this.state = {
-      data: [],
-      errors: {
-        firstName: false,
-        lastName: false,
-        date: false,
-        image: false,
-      },
-      msgHide: true,
-    };
+const validateTextField = (ref: React.RefObject<HTMLInputElement>): boolean => {
+  const regex = /\b[A-Z].*?\b/;
+
+  if (regex.test(ref.current?.value || '')) return true;
+  else if (ref.current) {
+    ref.current.value = '';
+  }
+  return false;
+};
+
+const validateDate = (ref: React.RefObject<HTMLInputElement>): boolean => {
+  const date = new Date(ref.current?.value || Date.now());
+  const diff = Date.now() - date.getTime();
+  const age = new Date(diff).getFullYear() - 1970;
+
+  if (age < 18) {
+    if (ref.current) {
+      ref.current.value = '';
+    }
+    return false;
+  }
+  return true;
+};
+
+const validateFile = (ref: React.RefObject<HTMLInputElement>): boolean => {
+  if (ref.current?.files && ref.current?.files[0]) {
+    return ref.current?.files[0].type === 'image/jpeg';
   }
 
-  private formRefs: FormRefs = {
-    firstName: createRef<HTMLInputElement>(),
-    lastName: createRef<HTMLInputElement>(),
-    date: createRef<HTMLInputElement>(),
-    sexMale: createRef<HTMLInputElement>(),
-    sexFemale: createRef<HTMLInputElement>(),
-    position: createRef<HTMLSelectElement>(),
-    rss: createRef<HTMLInputElement>(),
-    photo: createRef<HTMLInputElement>(),
+  return true;
+};
+
+const FormPage = () => {
+  const [data, setData] = useState<FormData[]>([]);
+  const [errors, setErrors] = useState<FormErrors>({
+    firstName: false,
+    lastName: false,
+    date: false,
+    image: false,
+  });
+
+  const [msgHide, setMsgHide] = useState(true);
+
+  const formRefs: FormRefs = {
+    firstName: useRef<HTMLInputElement>(null),
+    lastName: useRef<HTMLInputElement>(null),
+    date: useRef<HTMLInputElement>(null),
+    sexMale: useRef<HTMLInputElement>(null),
+    sexFemale: useRef<HTMLInputElement>(null),
+    position: useRef<HTMLSelectElement>(null),
+    rss: useRef<HTMLInputElement>(null),
+    photo: useRef<HTMLInputElement>(null),
   };
 
-  private formRef = createRef<HTMLFormElement>();
+  const formRef = useRef<HTMLFormElement>(null);
 
-  private onSubmitHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
+  const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    const errors: FormErrors = {
-      firstName: !this.validateTextField(this.formRefs.firstName),
-      lastName: !this.validateTextField(this.formRefs.lastName),
-      date: !this.validateDate(this.formRefs.date),
-      image: !this.validateFile(this.formRefs.photo),
+    const validateErrors: FormErrors = {
+      firstName: !validateTextField(formRefs.firstName),
+      lastName: !validateTextField(formRefs.lastName),
+      date: !validateDate(formRefs.date),
+      image: !validateFile(formRefs.photo),
     };
 
-    this.setState({
-      errors,
-    });
+    setErrors(validateErrors);
 
-    const isFormValid = Object.values(errors).every((error) => !error);
+    const isFormValid = Object.values(validateErrors).every((error) => !error);
 
     if (isFormValid) {
       let fileName = 'No file';
-      const files = this.formRefs.photo.current?.files;
+      const files = formRefs.photo.current?.files;
 
       if (files && files.length) {
         fileName = files[0].name;
       }
 
       const newData: FormData = {
-        firstName: this.formRefs.firstName.current?.value || '',
-        lastName: this.formRefs.lastName.current?.value || '',
-        date: this.formRefs.date.current?.value || '',
-        sex: this.formRefs.sexMale.current?.checked ? 'Male' : 'Female',
-        position: positionOptions[this.formRefs.position.current?.value || ''],
-        rss: this.formRefs.rss.current?.checked ? 'Yes' : 'No',
+        firstName: formRefs.firstName.current?.value || '',
+        lastName: formRefs.lastName.current?.value || '',
+        date: formRefs.date.current?.value || '',
+        sex: formRefs.sexMale.current?.checked ? 'Male' : 'Female',
+        position: positionOptions[formRefs.position.current?.value || ''],
+        rss: formRefs.rss.current?.checked ? 'Yes' : 'No',
         photo: fileName,
       };
 
-      this.formRef.current?.reset();
+      formRef.current?.reset();
 
-      this.setState({ data: [...this.state.data, newData], msgHide: false });
+      setData([...data, newData]);
+      setMsgHide(false);
 
       setTimeout(() => {
-        this.setState({ msgHide: true });
+        setMsgHide(true);
       }, 3000);
     }
   };
 
-  private validateTextField(ref: React.RefObject<HTMLInputElement>): boolean {
-    const regex = /\b[A-Z].*?\b/;
-
-    if (regex.test(ref.current?.value || '')) return true;
-    else if (ref.current) {
-      ref.current.value = '';
-    }
-    return false;
-  }
-
-  private validateDate(ref: React.RefObject<HTMLInputElement>): boolean {
-    const date = new Date(ref.current?.value || Date.now());
-    const diff = Date.now() - date.getTime();
-    const age = new Date(diff).getFullYear() - 1970;
-
-    if (age < 18) {
-      if (ref.current) {
-        ref.current.value = '';
-      }
-      return false;
-    }
-    return true;
-  }
-
-  private validateFile(ref: React.RefObject<HTMLInputElement>): boolean {
-    if (ref.current?.files && ref.current?.files[0]) {
-      return ref.current?.files[0].type === 'image/jpeg';
-    }
-
-    return true;
-  }
-
-  render(): React.ReactNode {
-    const cards = this.state.data.map((card, idx) => <CvCard key={idx.toString()} {...card} />);
-    return (
-      <>
-        <section className={styles.section}>
-          <form className={styles.form} onSubmit={this.onSubmitHandler} ref={this.formRef}>
+  const cards = data.map((card, idx) => <CvCard key={idx.toString()} {...card} />);
+  return (
+    <>
+      <section className={styles.section}>
+        <form className={styles.form} onSubmit={onSubmitHandler} ref={formRef}>
+          <label>
+            First name: <input type="text" name="first-name" required ref={formRefs.firstName} />
+          </label>
+          <Error
+            shouldDisplay={errors.firstName}
+            text="First name must start with a capital letter"
+          />
+          <label>
+            Last name: <input type="text" name="last-name" required ref={formRefs.lastName} />
+          </label>
+          <Error
+            shouldDisplay={errors.lastName}
+            text="Last name must start with a capital letter"
+          />
+          <label>
+            Birth date: <input type="date" name="date" ref={formRefs.date} required />
+          </label>
+          <Error shouldDisplay={errors.date} text="You must be elder than 18" />
+          <fieldset className={[styles.radioButtons, styles.formElementPadding].join(' ')}>
+            Sex:{' '}
             <label>
-              First name:{' '}
-              <input type="text" name="first-name" required ref={this.formRefs.firstName} />
+              Male
+              <input type="radio" name="sex" value="male" ref={formRefs.sexMale} required />
             </label>
-            <Error
-              shouldDisplay={this.state.errors.firstName}
-              text="First name must start with a capital letter"
-            />
             <label>
-              Last name:{' '}
-              <input type="text" name="last-name" required ref={this.formRefs.lastName} />
+              {'   '}Female
+              <input type="radio" name="sex" value="female" ref={formRefs.sexFemale} />
             </label>
-            <Error
-              shouldDisplay={this.state.errors.lastName}
-              text="Last name must start with a capital letter"
-            />
-            <label>
-              Birth date: <input type="date" name="date" ref={this.formRefs.date} required />
-            </label>
-            <Error shouldDisplay={this.state.errors.date} text="You must be elder than 18" />
-            <fieldset className={[styles.radioButtons, styles.formElementPadding].join(' ')}>
-              Sex:{' '}
-              <label>
-                Male
-                <input type="radio" name="sex" value="male" ref={this.formRefs.sexMale} required />
-              </label>
-              <label>
-                {'   '}Female
-                <input type="radio" name="sex" value="female" ref={this.formRefs.sexFemale} />
-              </label>
-            </fieldset>
-            <label className={styles.formElementPadding}>
-              Desired position:{' '}
-              <select name="position" defaultValue={''} ref={this.formRefs.position} required>
-                <option value="" disabled>
-                  – Choose position –
-                </option>
-                {Object.keys(positionOptions).map((key) => {
-                  return (
-                    <option value={key} key={key}>
-                      {positionOptions[key]}
-                    </option>
-                  );
-                })}
-              </select>
-            </label>
-            <label className={styles.formElementPadding}>
-              Graduated from RS School:{' '}
-              <input type="checkbox" name="rs-school" ref={this.formRefs.rss} />
-            </label>
-            Photo
-            <label className={styles.formElementPadding}>
-              <input type="file" name="photo" ref={this.formRefs.photo} accept="image/jpeg" />
-            </label>
-            <Error shouldDisplay={this.state.errors.image} text="Image must have jpeg format" />
-            <input type="submit" value="Submit" />
-          </form>
-        </section>
-        <ConfirmMsg isHide={this.state.msgHide} />
-        <section className={styles.cards}>{cards}</section>
-      </>
-    );
-  }
-}
+          </fieldset>
+          <label className={styles.formElementPadding}>
+            Desired position:{' '}
+            <select name="position" defaultValue={''} ref={formRefs.position} required>
+              <option value="" disabled>
+                – Choose position –
+              </option>
+              {Object.keys(positionOptions).map((key) => {
+                return (
+                  <option value={key} key={key}>
+                    {positionOptions[key]}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <label className={styles.formElementPadding}>
+            Graduated from RS School: <input type="checkbox" name="rs-school" ref={formRefs.rss} />
+          </label>
+          Photo
+          <label className={styles.formElementPadding}>
+            <input type="file" name="photo" ref={formRefs.photo} accept="image/jpeg" />
+          </label>
+          <Error shouldDisplay={errors.image} text="Image must have jpeg format" />
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+      <ConfirmMsg isHide={msgHide} />
+      <section className={styles.cards}>{cards}</section>
+    </>
+  );
+};
 
 export default FormPage;
