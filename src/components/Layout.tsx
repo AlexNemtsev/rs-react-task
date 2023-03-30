37077375f3dddde61b3dda24774553b0f2c390a0
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-03-30 11:17:41 +0000

    refactor: remove HOC

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,10 @@
 import Header from './header/Header';
-import WithRouter from './WithRouter';
 import { Outlet } from 'react-router-dom';
 
-// TODO: add useLocation here
 const Layout = () => {
-  const HeaderRouter = WithRouter(Header);
   return (
     <>
-      <HeaderRouter />
+      <Header />
       <main>
         <Outlet />
       </main>
