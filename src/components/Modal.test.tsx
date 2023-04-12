00a4de8d204b7b775921473648e2b993996b9ba7
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-04-12 13:09:24 +0300

    feat: add a test

--- src/components/Modal.test.tsx
+++ src/components/Modal.test.tsx
@@ -1,7 +1,7 @@
 import Modal from './Modal';
 import { render } from '@testing-library/react';
 import photosMock from '../../tests/tests-data/photos';
-import { vi } from 'vitest';
+import { test, vi } from 'vitest';
 
 const portalRoot = document.createElement('div');
 portalRoot.setAttribute('id', 'portal-root');
@@ -9,7 +9,7 @@ document.body.appendChild(portalRoot);
 
 test('Card rendered', async () => {
   const screen = render(<Modal photo={photosMock[0]} isOpen={true} handleClose={vi.fn()} />);
-  const image = await screen.findByAltText(photosMock[0].alt_description);
+  const image = screen.getByAltText(photosMock[0].alt_description);
   expect(image).toBeInTheDocument();
 
   const likes = await screen.findByText(`❤️${photosMock[0].likes}`);
@@ -17,4 +17,13 @@ test('Card rendered', async () => {
 
   expect(likes).toBeInTheDocument();
   expect(author).toBeInTheDocument();
+});
+
+test('should be unmounted', () => {
+  const { getByAltText, queryByAltText, unmount } = render(
+    <Modal photo={photosMock[0]} isOpen={true} handleClose={vi.fn()} />
+  );
+  expect(getByAltText(photosMock[0].alt_description)).toBeInTheDocument();
+  unmount();
+  expect(queryByAltText(photosMock[0].alt_description)).not.toBeInTheDocument();
 });
