0171a3c9d510861965523a41c64651d2e998856f
Alexander Nemtsev <mrcrazy696@gmail.com>
2024-03-27 14:15:30 +0300

    refactor: remove field

--- src/libs/loader.ts
+++ src/libs/loader.ts
@@ -1,13 +1,12 @@
 class UnsplashLoader {
   private static url = 'https://api.unsplash.com';
-  private static apiKey = import.meta.env.VITE_API_KEY;
 
   static getPhotos(search: string): Promise<Response> {
     const searchRoute = search ? `/search/photos?page=1&query=${search}` : '/photos';
 
     return fetch(`${UnsplashLoader.url}${searchRoute}`, {
       method: 'GET',
-      headers: { Authorization: `Client-ID ${UnsplashLoader.apiKey}` },
+      headers: { Authorization: `Client-ID ${import.meta.env.VITE_API_KEY}` },
     });
   }
 }
